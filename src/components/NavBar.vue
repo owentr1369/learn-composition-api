e7bc2af9d4e9788d71719cc8b702f19965337eab
owentr1369 <tamvaa1306@gmail.com>
2022-05-11 19:41:11 +0700

    reactivity with ref

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,12 @@
 <template>
   <nav class="nav">
-    <router-link to="/">Dashboard</router-link>
-    <router-link :to="{ name: 'transactions-route' }">Transactions</router-link>
-    <router-link to="/budgets">Budgets</router-link>
-    <router-link to="/reports">Reports</router-link>
-    <router-link to="/settings">Settings</router-link>
+    <router-link class="link" to="/">Over View</router-link>
+    <router-link class="link" :to="{ name: 'transactions-route' }"
+      >Transactions</router-link
+    >
+    <router-link class="link" to="/budgets">Budgets</router-link>
+    <router-link class="link" to="/reports">Reports</router-link>
+    <router-link class="link" to="/settings">Settings</router-link>
     <router-view />
   </nav>
 </template>
@@ -16,4 +18,13 @@ export default {
 </script>
 
 <style>
+.link {
+  padding: 10px;
+  background-color: #777;
+  margin: 4px;
+  border-radius: 6px;
+  text-decoration: none;
+  color: #fff;
+  font-weight: 700;
+}
 </style>
--- src/pages/OverView.vue
+++ src/pages/OverView.vue
@@ -1,10 +1,29 @@
 <template>
   <h1>OverView</h1>
+  <h2>{{ firstName }}</h2>
+  <button @click="onChangeSomething">Click Me</button>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "OverViewPage",
+  setup() {
+    const firstName = ref("Tony Stark");
+
+    function onChangeSomething() {
+      console.log("Running...");
+      console.log(firstName);
+      firstName.value = "Tran Tam";
+      // firstName = "Tran Tam";
+    }
+
+    return {
+      firstName,
+      onChangeSomething,
+    };
+  },
 };
 </script>
 

4e6375ccac4a73bf7688019fdc6bcc532f065c69
owentr1369 <tamvaa1306@gmail.com>
2022-05-11 19:21:22 +0700

    add base

---
+++ src/pages/TransactionsPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Transactions page</h1>
+  <div v-if="transactions.length">
+    <div class="item" v-for="transaction in transactions" :key="transaction.id">
+      <router-link
+        :to="{
+          name: 'transaction-details-route',
+          params: { id: transaction.id },
+        }"
+        >{{ transaction.name }}</router-link
+      >
+      <div class="price">Price: {{ transaction.price }}</div>
+    </div>
+  </div>
+  <div v-else>Loading transactions...</div>
+</template>
+
+<script>
+export default {
+  name: "TransactionsPage",
+  data() {
+    return {
+      transactions: [],
+    };
+  },
+  created() {
+    fetch("http://localhost:3000/transactions")
+      .then((response) => response.json())
+      .then((data) => (this.transactions = data));
+  },
+};
+</script>
+
+<style>
+</style>

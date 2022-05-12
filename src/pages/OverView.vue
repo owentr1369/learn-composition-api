f7a7d0361526e548e3df96927005acfe6b215344
owentr1369 <tamvaa1306@gmail.com>
2022-05-12 10:27:45 +0700

    watch and watchEffect

--- src/pages/OverView.vue
+++ src/pages/OverView.vue
@@ -9,13 +9,16 @@
 </template>
 
 <script>
-import { computed, reactive, ref } from "vue";
+import { watch, computed, reactive, ref, watchEffect } from "vue";
 
 export default {
   name: "OverViewPage",
   setup() {
     // ref nên dùng cho primitive type
     // reactive nên dùng cho object, array k cần truy cập .value
+    // computed chạy lại liên tục khi value thay đổi (có trả giá trị)
+    // watch dùng để theo dõi 1 biến trong quá trình làm việc
+    // watchEffect là computed phiên bản không yêu cầu trả dữ liệu
     const searchText = ref("");
     const customers = reactive(["Mec", "BMW", "Honda"]);
     const customersFilted = computed(() =>
@@ -26,6 +29,15 @@ export default {
         })
         .filter((customer) => customer.includes(searchText.value.toLowerCase()))
     );
+    watch(searchText, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
+    });
+    watchEffect(() => {
+      if (searchText.value) {
+        console.log("run again");
+      }
+    });
+
     return {
       searchText,
       customersFilted,

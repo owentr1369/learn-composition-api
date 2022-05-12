eeee590e8e88040bc2c33cd8bebf283fbce77a42
owentr1369 <tamvaa1306@gmail.com>
2022-05-12 10:15:00 +0700

    computed in composition api

--- src/pages/OverView.vue
+++ src/pages/OverView.vue
@@ -1,44 +1,34 @@
 <template>
   <h1>OverView</h1>
-  <h2>{{ secondName }}</h2>
-  <p>{{ carInfo }}</p>
-  <button @click="onChangeSomething">Click Me</button>
+  <input type="text" v-model="searchText" />
+  <ul>
+    <li v-for="(customer, index) in customersFilted" :key="index">
+      {{ customer }}
+    </li>
+  </ul>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 
 export default {
   name: "OverViewPage",
   setup() {
     // ref nên dùng cho primitive type
     // reactive nên dùng cho object, array k cần truy cập .value
-    const firstName = ref("Tony Stark");
-    let carInfo = reactive({
-      price: 100000,
-      name: "Mec",
-    });
-    const secondName = ref({
-      name: "Hu",
-      something: "Ha",
-    });
-
-    function onChangeSomething() {
-      secondName.value = {
-        name: "Tran Tam",
-        something: "Sharing",
-      };
-      carInfo = {
-        price: 20,
-        name: "BMW",
-      };
-    }
-
+    const searchText = ref("");
+    const customers = reactive(["Mec", "BMW", "Honda"]);
+    const customersFilted = computed(() =>
+      customers
+        .map((customer) => {
+          customer = customer.toLowerCase();
+          return customer;
+        })
+        .filter((customer) => customer.includes(searchText.value.toLowerCase()))
+    );
     return {
-      firstName,
-      carInfo,
-      secondName,
-      onChangeSomething,
+      searchText,
+      customersFilted,
     };
   },
 };
